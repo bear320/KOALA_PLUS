1b72e507e9a5056eee563c9b2fb285666db069e0
chiiiiilin <chei94117@gmail.com>
2022-11-28 20:00:19 +0800

    Merge branch 'dev' of https://github.com/bear320/cgd103_g1 into dev
    # Conflicts:
    #	package-lock.json
    #	src/assets/sass/layout/mixin/_mixin.scss
    #	src/assets/sass/style.scss

---
+++ src/views/BackEndView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">我是後台</div>
+</template>
+
+<script>
+// @ is an alias to /src
+/* import HelloWorld from "@/components/HelloWorld.vue"; */
+
+export default {
+    /* name: "HomeView",
+    components: {
+        HelloWorld,
+    }, */
+};
+</script>
