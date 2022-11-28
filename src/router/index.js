f892c7b6f55a4b945205955042ac7302744b6a1a
bear320 <aaabear320@gmail.com>
2022-11-28 22:40:26 +0800

    add xxx.vue 新增頁面

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
-import IndxView from "../views/IndxView.vue";
+import IndexView from "../views/IndexView.vue";
 import HomeView from "../views/HomeView.vue";
-import BackEndView from "../views/BackEndView.vue";
+import BackendView from "../views/BackendView.vue";
+import About from "../views/About.vue";
+import AboutKoala from "../views/AboutKoala.vue";
+import AllKoalas from "../views/AllKoalas.vue";
+import Cart1 from "../views/Cart1.vue";
+import Cart2 from "../views/Cart2.vue";
+import KoalaInfo from "../views/KoalaInfo.vue";
+import KoalaPark from "../views/KoalaPark.vue";
+import Login from "../views/Login.vue";
+import Member from "../views/Member.vue";
+import MyKoala from "../views/MyKoala.vue";
+import Product from "../views/Product.vue";
+import Shop from "../views/Shop.vue";
 
 const routes = [
     {
-            path: "/",
-            name: "index",
-        component: IndxView,
+        path: "/",
+        name: "index",
+        component: IndexView,
     },
     {
         path: "/home",
@@ -17,7 +29,67 @@ const routes = [
     {
         path: "/backend",
         name: "backend",
-        component: BackEndView,
+        component: BackendView,
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: About,
+    },
+    {
+        path: "/about-koala",
+        name: "about-koala",
+        component: AboutKoala,
+    },
+    {
+        path: "/all-koalas",
+        name: "all-koalas",
+        component: AllKoalas,
+    },
+    {
+        path: "/cart1",
+        name: "cart1",
+        component: Cart1,
+    },
+    {
+        path: "/cart2",
+        name: "cart2",
+        component: Cart2,
+    },
+    {
+        path: "/koala-info",
+        name: "koala-info",
+        component: KoalaInfo,
+    },
+    {
+        path: "/koala-park",
+        name: "koala-park",
+        component: KoalaPark,
+    },
+    {
+        path: "/login",
+        name: "long",
+        component: Login,
+    },
+    {
+        path: "/member",
+        name: "member",
+        component: Member,
+    },
+    {
+        path: "/my-koala",
+        name: "my-koala",
+        component: MyKoala,
+    },
+    {
+        path: "/product",
+        name: "product",
+        component: Product,
+    },
+    {
+        path: "/shop",
+        name: "shop",
+        component: Shop,
     },
 ];
 
---
+++ src/views/IndexView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <router-link to="/home">前台</router-link>
+        <router-link to="/backend">後台</router-link>
+        <router-link to="/about">關於</router-link>
+        <router-link to="/about-koala">無尾熊百科</router-link>
+        <router-link to="/all-koalas">資助認養</router-link>
+        <router-link to="/cart1">購物車1</router-link>
+        <router-link to="/cart2">購物車2</router-link>
+        <router-link to="/koala-info">無尾熊詳情頁</router-link>
+        <router-link to="/koala-park">園區資訊</router-link>
+        <router-link to="/login">登入註冊</router-link>
+        <router-link to="/member">會員專區</router-link>
+        <router-link to="/my-koala">我的無尾熊</router-link>
+        <router-link to="/product">產品詳情</router-link>
+        <router-link to="/shop">周邊商城</router-link>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+a {
+    display: block;
+}
+</style>
