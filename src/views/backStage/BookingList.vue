b33fa59526316eb467dd6eea5c9e4c09672d810a
Nate <natehuang0913@gmail.com>
2022-12-07 00:08:11 +0800

    add backend-bookingListVue

--- src/views/backStage/BookingList.vue
+++ src/views/backStage/BookingList.vue
@@ -3,42 +3,41 @@
     <article class="bs-nav-space wrapper">
         <h1>園區導覽管理</h1>
         <div class="todo">
-            <a href="/bs-article-edit" target="_blank"
+            <a href="#" target="_blank"
                 ><img src="@/assets/images/icon/FilePlus.svg" alt=""
             /></a>
             <select name="category" id="articlesCategory">
-                <option value="all">所有文章</option>
-                <option value="news">最新消息</option>
-                <option value="parkinfo">園區資訊</option>
-                <option value="moneyinfo">資金運用</option>
+                <option value="all">預約訂單</option>
+                <option value="day-off">休館訂單</option>
             </select>
         </div>
         <section>
             <h2>預約總覽</h2>
             <div class="bstitle">
-                <h3 class="star">訂單編號</h3>
-                <h3 class="article">姓名</h3>
-                <h3 class="date">日期</h3>
-                <h3 class="tag">分類</h3>
-                <h3 class="operator">操作</h3>
+                <h3 class="ord-no">訂單編號</h3>
+                <h3 class="name">姓名</h3>
+                <h3 class="date">預約日期</h3>
+                <h3 class="ppl">人數</h3>
+                <h3 class="connect">Email/電話</h3>
+                <h3 class="status">備註</h3>
+                <h3 class="operator">編輯</h3>
             </div>
-            <div class="content" v-for="article in articles" :key="article">
-                <p class="star"></p>
-                <div class="article">
-                    <p>{{ article.title }}</p>
-                    <p>{{ article.content }}</p>
-                </div>
-                <div class="pic"><img :src="article.img" alt="" /></div>
-                <p class="date">{{ article.date }}</p>
-                <p class="tag">{{ article.tag }}</p>
-                <div class="operator">
-                    <img src="@/assets/images/icon/upload.svg" alt="" />
+            <div class="content" v-for="list in orders" :key="list">
+                <p class="ord-no">{{ list.number }}</p>
+                <p class="name">
+                    {{ list.name }}
+                </p>
+                <p class="date">{{ list.date }}</p>
+                <p class="ppl">{{ list.ppl }}</p>
+                <p class="connect">
+                    <span class="email"> {{ list.email }}</span>
+                    <span class="moible"> {{ list.mobile }}</span>
+                </p>
+                <p class="status">{{ list.status }}</p>
+                <p class="operator">
                     <img src="@/assets/images/icon/edit.svg" alt="" />
-                </div>
+                </p>
             </div>
-        </section>
-        <section>
-            <h2>已上傳</h2>
         </section>
     </article>
 </template>
@@ -51,22 +50,33 @@ export default {
     },
     data() {
         return {
-            articles: [
+            orders: [
                 {
-                    img: require("@/assets/images/about/3.jpg"),
-                    title: "一隻無尾熊寶寶出生了",
-                    tag: "#最新消息",
-                    date: "2022/12/25",
-                    content:
-                        "每年的9到11月是無尾熊開始繁殖的交配季節，當無尾熊長到2歲時就開始邁入性成熟年齡、可以進行傳宗接代的重責大任。無尾熊平均一胎以孕育一隻baby為主，偶爾會有雙胞胎出現，平均壽命為10至12歲。「為了迎接三隻無尾熊寶寶誕生，動物園特別精心打造了育嬰房’」趙明杰笑著說，為讓......",
+                    number: 1122,
+                    name: "Sara",
+                    ppl: 5,
+                    date: "2022-12-25",
+                    status: "已預約",
+                    email: "sara168@gmail.comn",
+                    mobile: "0928168168",
                 },
                 {
-                    img: require("@/assets/images/about/8.jpg"),
-                    title: "園區公休通知",
-                    tag: "#園區消息",
-                    date: "2022/12/13",
-                    content:
-                        "因應園區大清潔及設施維修工程，KOALA+園區將於112/02/15~112/02/20公休6日，造成不便盡請見諒。",
+                    number: 1125,
+                    name: "Amy",
+                    ppl: 15,
+                    date: "2022-12-13",
+                    email: "amy888@gmail.comn",
+                    mobile: "0928888888",
+                    status: "已預約",
+                },
+                {
+                    number: 1231,
+                    name: "休館",
+                    ppl: 0,
+                    date: "2022-12-31",
+                    email: "",
+                    mobile: "",
+                    status: "休館",
                 },
             ],
         };
@@ -100,18 +110,18 @@ html article {
             margin-bottom: 0;
             padding-left: 10px;
         }
-        .star {
-            width: 5%;
+        .ord-no {
+            width: 10%;
         }
-        .article {
-            width: 45%;
+        .name {
+            width: 10%;
 
             p:first-child {
                 font-weight: 700;
                 font-size: $h4;
             }
         }
-        .pic,
+
         .date {
             width: 15%;
 
@@ -121,7 +131,7 @@ html article {
             }
         }
 
-        .tag,
+        .ppl,
         .operator {
             width: 10%;
             display: flex;
@@ -134,6 +144,14 @@ html article {
                 }
             }
         }
+        .connect {
+            width: 20%;
+            display: flex;
+            flex-direction: column;
+        }
+        .status {
+            width: 10%;
+        }
         .bstitle {
             display: flex;
             width: 100%;
@@ -143,11 +161,15 @@ html article {
         }
 
         .content {
+            text-align: center;
             display: flex;
             align-items: center;
             padding: 10px 0;
             margin-bottom: 10px;
             border-bottom: solid 1px $lightgreen;
+            p {
+                text-align: center;
+            }
         }
     }
 }
