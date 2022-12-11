84f25810dbf5ff5c6d1f81bc2d3099c1cf7d4a69
igor <igor@gmail.com>
2022-12-11 10:55:10 +0800

    modify 修改 會員認養紀錄的canves modify 會員中心 firstname lastname 統一改成 mem_name 合併再一起 modify 會員 領養紀錄欄未調整為根領養後台一致

--- src/components/member/MemAdopt.vue
+++ src/components/member/MemAdopt.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <li class="mem_adopt content">
         <div class="mem_basic">
-            <p>名字：{{ mem_name }}</p>
+            <p>姓名：{{ mem_name }}</p>
             <p>會員ID：{{ mem_id }}</p>
         </div>
         <div class="mem_main">
@@ -10,9 +10,10 @@
                 <table class="table">
                     <thead>
                         <tr>
-                            <th>無尾熊名字</th>
-                            <th class="koala_id">無尾熊編號</th>
-                            <th class="sup_date">資助認養時間</th>
+                            <th class="sup_id">資助認養編號</th>
+                            <th class="sup_date">資助認養日期</th>
+                            <th class="sup_plan">資助認養方案</th>
+                            <th class="koala_name">無尾熊名字</th>
                             <th class="sup_price">資助認養金額</th>
                             <th>下載領養證明</th>
                         </tr>
@@ -20,17 +21,19 @@
                     <tbody>
                         <tr
                             v-for="(item, index) in couponsList"
-                            :key="item.koala_id"
+                            :key="item.sup_id"
                         >
-                            <td>{{ item.koala_name }}</td>
-                            <td class="koala_id">{{ item.koala_id }}</td>
+                            <td class="sup_id">{{ item.sup_id }}</td>
                             <td class="sup_date">{{ item.sup_date }}</td>
+                            <td class="sup_plan">{{ item.sup_plan }}</td>
+                            <td class="koala_name">{{ item.koala_name }}</td>
                             <td class="sup_price">{{ item.sup_price }}</td>
                             <td>
                                 <Icon
                                     type="md-download"
                                     size="20"
                                     color="#337a7d"
+                                    @click="showcouponsList(index)"
                                 />
                             </td>
                         </tr>
@@ -38,20 +41,27 @@
                 </table>
             </div>
         </div>
-        <!-- <p class="certificate_date">{{ couponsList.sup_date }}</p> -->
 
         <div>
             <div class="html2canvas" ref="html2canvas">
                 <!-- 轉出來的證書 -->
+                <!-- <div class="certificate_img"> -->
                 <div class="adopt_certificate">
+                    <img
+                        class="certificatebg"
+                        src="@/assets/images/member/Certificate01.svg"
+                        alt="會員頁面的無尾熊左圖"
+                    />
                     <p class="certificate_name">{{ mem_name }}</p>
-                    <!-- <div  v-for="(item, index) in couponsList" 
-                    :key="item.koala_name>
-                        <p class="certificate_koala">
-                        {{ item.koala_name }}
+
+                    <p class="certificate_koala">
+                        {{ couponsList[certificate_Index].koala_name }}
                     </p>
-                    </div> -->
+                    <p class="certificate_date">
+                        {{ couponsList[certificate_Index].sup_date }}
+                    </p>
                 </div>
+                <!-- </div> -->
             </div>
             <img :src="imgUrl" />
         </div>
@@ -63,33 +73,38 @@ import html2canvas from "html2canvas";
 export default {
     data() {
         return {
-            mem_name: "Charmy",
+            mem_name: "曾韋翰",
             mem_id: "P00001",
             imgUrl: "",
+            certificate_Index: 0,
             couponsList: [
                 {
-                    koala_name: "Susan",
-                    koala_id: "#KOALA0001",
-                    sup_date: "2022/12/01",
-                    sup_price: "$1000",
-                },
-                {
-                    koala_name: "Alen",
-                    koala_id: "#KOALA0002",
-                    sup_date: "2022/12/21",
-                    sup_price: "$1000",
-                },
-                {
-                    koala_name: "Duffy",
-                    koala_id: "#KOALA0003",
-                    sup_date: "2022/11/19",
-                    sup_price: "$300",
-                },
-                {
-                    koala_name: "jenny",
-                    koala_id: "#KOALA0004",
+                    sup_id: "S00001",
                     sup_date: "2022/11/15",
+                    sup_plan: "認養",
+                    koala_name: "jennykoala",
+                    sup_price: "$1000",
+                },
+                {
+                    sup_id: "S00002",
+                    sup_date: "2022/12/01",
+                    sup_plan: "資助",
+                    koala_name: "",
                     sup_price: "$300",
+                },
+                {
+                    sup_id: "S00003",
+                    sup_date: "2022/12/21",
+                    sup_plan: "資助",
+                    koala_name: "",
+                    sup_price: "$300",
+                },
+                {
+                    sup_id: "S00004",
+                    sup_date: "2022/11/19",
+                    sup_plan: "認養",
+                    koala_name: "Duffy",
+                    sup_price: "$1000",
                 },
             ],
         };
@@ -110,6 +125,10 @@ export default {
                 // 可以上传后端或者直接显示
                 this.imgUrl = url;
             });
+        },
+        showcouponsList(index) {
+            console.log(index);
+            this.certificate_Index = index;
         },
     },
 };
@@ -140,13 +159,13 @@ table {
         th {
             padding: 10px;
         }
-        .koala_id {
+        .sup_id {
             @include m() {
                 display: none;
             }
         }
         .sup_date {
-            @include m() {
+            @include d() {
                 display: none;
             }
         }
@@ -158,35 +177,52 @@ table {
     }
 }
 // 這是SHOW出來的圖片
+
 .adopt_certificate {
-    position: relative;
     width: 600px;
     height: 426px;
+    position: relative;
     // background-color: pink;
-    background-image: url("@/assets/images/member/Certificate01.png");
+    // background-image: url("@/assets/images/member/Certificate01.svg");
+    .certificatebg {
+        width: 100%;
+        height: 100%;
+    }
     .certificate_name {
+        position: relative;
+        width: fit-content;
+        background-color: red;
+        top: 29%;
+        left: 50%;
+    }
+    .certificate_koala {
+        position: relative;
+    }
+
+    .certificate_date {
         position: relative;
     }
 }
-#capture {
-    margin: 0 auto;
-    width: 100%;
-    height: 400px;
-    overflow: hidden;
-}
-textarea {
-    width: 100%;
-    padding: 2rem 0.5rem;
-    background: transparent;
-    border: 0;
-    text-align: center;
-    color: #fff;
-    font-family: "Noto Sans", sans-serif;
-    font-weight: 700;
-    font-size: 2rem;
-    resize: none;
-    overflow: hidden;
-    max-height: 100%;
-    min-height: 100%;
-}
+
+// #capture {
+//     margin: 0 auto;
+//     width: 100%;
+//     height: 100%;
+//     overflow: hidden;
+// }
+// textarea {
+//     width: 100%;
+//     padding: 2rem 0.5rem;
+//     background: transparent;
+//     border: 0;
+//     text-align: center;
+//     color: #fff;
+//     // font-family: "Noto Sans", sans-serif;
+//     font-weight: 700;
+//     font-size: 2rem;
+//     resize: none;
+//     overflow: hidden;
+//     max-height: 100%;
+//     min-height: 100%;
+// }
 </style>
