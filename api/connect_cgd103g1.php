af93ba676f51f6fe468d8191043c27b2b58da6a9
Nate <natehuang0913@gmail.com>
2022-12-13 13:26:29 +0800

    connect book chang(?

--- api/connect_cgd103g1.php
+++ api/connect_cgd103g1.php
@@ -1,8 +1,8 @@
 <?php
     $dbname = "tibamefe_cgd103g1";
 	$user = "root";
-	$password = "root";
-	$port="8888";
+	$password = "relax0913";
+	$port="3306";
 	$dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";
 
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
