52d7d91250bc9108c9af84e9aae7b13e819b47da
bear320 <aaabear320@gmail.com>
2022-12-22 17:29:24 +0800

    modify getAllOrders.php / OrderList.vue 完成訂單管理搜尋＆分頁功能

--- src/views/backStage/OrderList.vue
+++ src/views/backStage/OrderList.vue
@@ -1,10 +1,19 @@
 <template>
     <Header />
     <article class="nav-space wrapper">
-        <h1>訂單管理</h1>
-        <div class="todo">
-            <div class="search">
-                <input type="search" name="" id="" placeholder="搜尋" />
+        <div class="title-n-action">
+            <h1>訂單管理</h1>
+            <div class="todo">
+                <div class="search">
+                    <input
+                        type="search"
+                        name="search"
+                        id="search"
+                        placeholder="搜尋訂單編號"
+                        v-model.trim="search"
+                        @input="changeVal"
+                    />
+                </div>
             </div>
         </div>
         <section>
@@ -120,6 +129,20 @@
             </div>
         </section>
     </article>
+    <section class="pagination">
+        <ul>
+            <a @click="prePage"><li>&lt;</li></a>
+            <a
+                v-for="i in totalPage"
+                :class="{ 'is-active': currentPage == i }"
+                :key="i"
+                href="#"
+                @click="changePage(i)"
+                ><li>{{ i }}</li></a
+            >
+            <a @click="nextPage"><li>></li></a>
+        </ul>
+    </section>
 </template>
 
 <script>
@@ -130,6 +153,9 @@ export default {
         return {
             orders: [],
             orderlists: [],
+            search: "",
+            currentPage: this.$route.query.page ? this.$route.query.page : 1,
+            totalPage: 0,
         };
     },
     computed: {
@@ -140,17 +166,34 @@ export default {
             }, {});
         },
     },
+    watch: {
+        $route: function () {
+            if (!Object.keys(this.$route.query).length) {
+                this.currentPage = 1;
+                this.getAllOrders(this.$route.query);
+            }
+            this.getAllOrders(this.$route.query);
+        },
+    },
     methods: {
-        getAllOrders() {
+        getAllOrders(queryParam) {
+            if (!Object.keys(queryParam).length) {
+                queryParam.limit = 10;
+                queryParam.page = 1;
+                queryParam.search = this.search;
+            }
             // const apiURL = new URL(
             //     "http://localhost:8888/cgd103_g1/public/api/getAllOrders.php"
             // );
             const apiURL = new URL(`${BASE_URL}/getAllOrders.php`);
+            const searchParam = new URLSearchParams(queryParam);
+            apiURL.search = searchParam;
             fetch(apiURL)
                 .then((res) => res.json())
                 .then((json) => {
-                    console.log(json);
-                    this.orders = json;
+                    console.log("test", json);
+                    this.orders = json.orders;
+                    this.totalPage = Math.ceil(json.orderCount / 10);
                 });
         },
         getOrderLists() {
@@ -161,7 +204,7 @@ export default {
             fetch(apiURL)
                 .then((res) => res.json())
                 .then((json) => {
-                    console.log(json);
+                    // console.log(json);
                     this.orderlists = json;
                 });
         },
@@ -184,9 +227,60 @@ export default {
                     alert(status.msg);
                 });
         },
+        changeVal() {
+            this.$router.push({
+                path: "/bs-order-list",
+                query: {
+                    limit: `10`,
+                    page: this.currentPage,
+                    search: this.search,
+                },
+            });
+        },
+        prePage() {
+            if (this.currentPage == 1) {
+                return;
+            } else {
+                this.currentPage--;
+                this.$router.push({
+                    path: "/bs-order-list",
+                    query: {
+                        limit: `10`,
+                        page: this.currentPage,
+                        search: this.search,
+                    },
+                });
+            }
+        },
+        nextPage() {
+            if (this.currentPage === this.totalPage) {
+                return;
+            } else {
+                this.currentPage++;
+                this.$router.push({
+                    path: "/bs-order-list",
+                    query: {
+                        limit: `10`,
+                        page: this.currentPage,
+                        search: this.search,
+                    },
+                });
+            }
+        },
+        changePage(page) {
+            this.currentPage = page;
+            this.$router.push({
+                path: "/bs-order-list",
+                query: {
+                    limit: `10`,
+                    page: this.currentPage,
+                    search: this.search,
+                },
+            });
+        },
     },
     created() {
-        this.getAllOrders();
+        this.getAllOrders(this.$route.query);
         this.getOrderLists();
     },
     components: {
@@ -196,8 +290,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-    margin-bottom: 50px;
+// section {
+//     margin-bottom: 50px;
+// }
+.title-n-action {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 .accordion {
     margin-top: 30px;
@@ -386,5 +485,30 @@ html article {
 }
 .bottom {
     padding-bottom: 50px;
+}
+.pagination {
+    padding: 30px 0;
+    a {
+        display: inline-block;
+        padding: 10px 18px;
+        color: $darkgreen;
+        //
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        padding: 0;
+        text-align: center;
+        &:hover {
+            color: $darkgreen;
+        }
+        &:focus {
+            color: #fff;
+        }
+    }
+    a.is-active {
+        background-color: $darkgreen;
+        border-radius: 100%;
+        color: #fff;
+    }
 }
 </style>
