527c49f50c59daa7db8faaf1ade4ffeb8164217a
bear320 <aaabear320@gmail.com>
2022-12-23 14:06:26 +0800

    無尾熊數據統計

--- src/views/backStage/SupportStatistic.vue
+++ src/views/backStage/SupportStatistic.vue
@@ -13,7 +13,46 @@
         </div>
         <div class="sub-title">
             <h2>數據統計 - 認養單隻無尾熊</h2>
-            <div class="chart-wrapper"></div>
+            <Tabs class="tab-wrapper">
+                <TabPane class="tab" label="Zeus" name="Zeus">
+                    <Line v-if="loaded" :data="dataSupAll" :options="options" />
+                </TabPane>
+                <TabPane class="tab" label="Hera" name="Hera"></TabPane>
+                <TabPane class="tab" label="Poseidon" name="Poseidon"></TabPane>
+                <TabPane class="tab" label="Ares" name="Ares"></TabPane>
+                <TabPane
+                    class="tab"
+                    label="Aphrodite"
+                    name="Aphrodite"
+                ></TabPane>
+                <TabPane class="tab" label="Apollo" name="Apollo"></TabPane>
+                <TabPane class="tab" label="Athena" name="Athena"></TabPane>
+                <TabPane class="tab" label="Hermes" name="Hermes"></TabPane>
+            </Tabs>
+            <br />
+            <!-- 分隔線 -->
+            <select
+                name="adoptStat"
+                id="adoptStat"
+                v-model="koalaSelect"
+                @change="optSelected(index)"
+            >
+                <option
+                    :value="koalaOpt.koala_name"
+                    v-for="(koalaOpt, index) in koalaName"
+                    :key="index"
+                >
+                    {{ koalaOpt.koala_name }}
+                </option>
+            </select>
+            <div v-for="(koalaChart, index) in koalaName" :key="index">
+                <div
+                    v-show="koalaSelect === koalaChart.koala_name"
+                    class="chart-wrapper"
+                >
+                    <h1>{{ koalaChart.koala_name }}</h1>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -67,6 +106,10 @@ export default {
                     },
                 ],
             },
+            dataAdoptKoala: {
+                labels: ["上上月", "上月", "本月"],
+                datasets: [],
+            },
             options: {
                 responsive: true,
                 plugins: {
@@ -77,7 +120,10 @@ export default {
                     // },
                 },
             },
-            chartdata: [],
+            source: [],
+            koalaName: [],
+            koalaSelect: "Zeus",
+            isSelected: 0,
         };
     },
     // methods: {
@@ -113,18 +159,65 @@ export default {
             )
                 .then((res) => res.json())
                 .then((json) => {
-                    console.log(json);
-                    this.dataSupAll.datasets[0].data = [
+                    /* this.dataSupAll.datasets[0].data = [
                         json[0]["koala_sum_the_month_before_last"],
                         json[0]["koala_sum_last_month"],
                         json[0]["koala_sum_this_month"],
-                    ];
+                    ]; */
+                    this.dataSupAll["datasets"] = json.map((array, index) => {
+                        return {
+                            label: "資助所有無尾熊",
+                            data: array,
+                            borderColor: "#33717d",
+                            backgroundColor: "#9abdbf",
+                            pointStyle: "circle",
+                            pointRadius: 10,
+                            pointHoverRadius: 15,
+                            cubicInterpolationMode: "monotone",
+                            tension: 0.4,
+                        };
+                    });
+                    // console.log(this.dataSupAll, this.dataSupAll["datasets"]);
+                });
+
+            //     dataSupAll: {
+            //     labels: ["上上月", "上月", "本月"],
+            //     datasets: [
+            //         {
+            //             label: "資助所有無尾熊",
+            //             data: [null, null, null],
+            //             borderColor: "#33717d",
+            //             backgroundColor: "#9abdbf",
+            //             pointStyle: "circle",
+            //             pointRadius: 10,
+            //             pointHoverRadius: 15,
+            //             cubicInterpolationMode: "monotone",
+            //             tension: 0.4,
+            //         },
+            //     ],
+            // },
+
+            await fetch(
+                `http://localhost:8888/cgd103_g1/public/api/getAdoptStat.php`
+            )
+                .then((res) => res.json())
+                .then((json) => {
+                    this.source = json;
+                    this.koalaName = json.adoptNameStat;
+                    console.log(this.source);
+                    console.log(this.source["adoptNameStat"][0]);
                 });
 
             this.loaded = true;
         } catch (e) {
             console.error(e);
         }
+    },
+    methods: {
+        optSelected(i) {
+            console.log("QQ", i);
+            this.isSelected = i;
+        },
     },
 };
 </script>
@@ -164,9 +257,40 @@ export default {
             justify-content: center;
             align-items: center;
         }
+        .tab-wrapper {
+            .tab {
+                width: 100%;
+                height: 500px;
+                background-color: #fff;
+                border: 1px solid $btn-light-color;
+                border-radius: 10px;
+                box-shadow: 0px 2px 2px 2px rgba(149, 149, 149, 0.2);
+                box-sizing: border-box;
+                padding: 20px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        }
     }
-    .sub-title + .sub-title {
-        margin-top: 50px;
+    .sub-title {
+        margin-bottom: 50px;
     }
+}
+</style>
+<style lang="scss">
+.ivu-tabs-nav {
+    .ivu-tabs-ink-bar {
+        display: none;
+    }
+    .ivu-tabs-tab:hover,
+    .ivu-tabs-tab.ivu-tabs-tab-active.ivu-tabs-tab-focused {
+        color: $btn-color;
+        font-weight: 700;
+    }
+}
+.ivu-tabs-bar {
+    margin-bottom: 5px;
+    border: none;
 }
 </style>
