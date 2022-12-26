ed1a27518bf245277ae92d88de6c2c0d2b1b67fe
chiiiiilin <chei94117@gmail.com>
2022-12-26 16:11:12 +0800

    modify articlelist 分類帽

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,5 +1,5 @@
 // 測tibame用
-export const BASE_URL = "https://tibamef2e.com/cgd103/g1/api";
+// export const BASE_URL = "https://tibamef2e.com/cgd103/g1/api";
 // 測Local端用
-// export const BASE_URL = "http://localhost/cgd103_g1/public/api";
+export const BASE_URL = "http://localhost/cgd103_g1/public/api";
 // Mac可能要自已補唷><
