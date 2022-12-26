4ff7cd0ae4561d665a74caed8f1876388037f431
bear320 <aaabear320@gmail.com>
2022-12-26 20:58:52 +0800

    modify Header.vue 修改後台 Header

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,5 +1,5 @@
-// 測tibame用
-// export const BASE_URL = "https://tibamef2e.com/cgd103/g1/api";
-// 測Local端用
-export const BASE_URL = "http://localhost/cgd103_g1/public/api";
+// 測 TibaMe 用
+export const BASE_URL = "https://tibamef2e.com/cgd103/g1/api";
+// 測 Localhost 用
+// export const BASE_URL = "http://localhost/cgd103_g1/public/api";
 // Mac可能要自已補唷><
